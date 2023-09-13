09d8ff9a011541fc35f4fdc71e2d80c173acf354
Anouk.Migliavacca <anouk.migliavacca@insa-rennes.fr>
2023-09-13 13:22:57 +0000

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 8494b7d646f66528ac41ad921f04ceca24eca5b1

--- client/actionnable_mockup/src/app/app.module.ts
+++ client/actionnable_mockup/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageSignInComponent } from './components/page-sign-in/page-sign-in.component';
-import { PageSignUpComponent } from './components/page-sign-up/page-sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageSignInComponent,
-    PageSignUpComponent
+    PageSignInComponent
   ],
   imports: [
     BrowserModule,
