999b35b45096af496a04a7571dc4078bdefdf990
Anouk.Migliavacca <anouk.migliavacca@insa-rennes.fr>
2023-09-13 13:25:52 +0000

    Revert "Creation of wignIn_page component"
    This reverts commit 551630b12fa6b2c260972b059387649dc23de428

--- client/actionnable_mockup/src/app/app.module.ts
+++ client/actionnable_mockup/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PageSignInComponent } from './components/page-sign-in/page-sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PageSignInComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
