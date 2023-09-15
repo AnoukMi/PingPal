df1d83570fe94d3f2194a14e57bcaf8508e54ff0
Hoa <leveilleambre05@gmail.com>
2023-09-15 23:15:12 +0200

    création du design de certains components

--- client/actionnable_mockup/src/app/components/user-bubble/user-bubble.component.ts
+++ client/actionnable_mockup/src/app/components/user-bubble/user-bubble.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-user-bubble',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-bubble.component.css']
 })
 export class UserBubbleComponent {
-
+    @Input() content: string = '';
 }
