599d42f93eccf4c24c9cfc7611851ac60bfbe942
Hoa <leveilleambre05@gmail.com>
2023-09-19 22:19:46 +0200

    added the shared message functionality

--- client/actionnable_mockup/src/app/components/page-share-message/page-share-message.component.ts
+++ client/actionnable_mockup/src/app/components/page-share-message/page-share-message.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Contact} from "../../models/contact";
 
 @Component({
   selector: 'app-page-share-message',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-share-message.component.css']
 })
 export class PageShareMessageComponent {
-
+  myself: Contact = new Contact("Ambre", "Léveillé",
+    "assets/avatar/f3.png", "ivambre", new Date(2003, 10, 5))
 }
