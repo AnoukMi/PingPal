fd257aacce6cca2900315a9c4f545752a70fc03f
anouk.mi <anouk.migliavacca@insa-rennes.fr>
2023-09-23 17:24:16 +0300

    details

--- client/actionnable_mockup/src/app/models/contact.ts
+++
@@ -1,5 +0,0 @@
-export class Contact {
-  constructor(public firstName: string = 'undefined', public lastName: string = 'undefined',
-              public avatar: string = 'undefined', public username: string = 'undefined',
-              public birthday: Date = new Date()) {}
-}
