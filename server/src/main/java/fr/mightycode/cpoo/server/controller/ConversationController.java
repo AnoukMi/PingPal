fc9a717809a6b6f1044346552a1f5638d8d06f0a
Anouk <anouk.migliavacca@insa-rennes.fr>
2023-10-04 09:44:54 +0300

    Test 1 sur nouvelle conversation (route conversation/{user}) et controller ConversationController

--- server/src/main/java/fr/mightycode/cpoo/server/controller/ConversationController.java
+++ server/src/main/java/fr/mightycode/cpoo/server/controller/ConversationController.java
@@ -1,12 +1,33 @@
-//@RestController
-//@RequestMapping("user")
-//@AllArgsConstructor
-//@CrossOrigin
-//public class ConversationController {
-//    private final ConversationService conversationService;
-//
-//    @PostMapping(value = "conversation/{user}", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public void createEmptyConv(){
-//        conversationService.createEmpty();
-//    }
-//}
+package fr.mightycode.cpoo.server.controller;
+
+import fr.mightycode.cpoo.server.dto.UserDTO;
+import fr.mightycode.cpoo.server.service.UserService;
+import jakarta.servlet.ServletException;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("user")
+@AllArgsConstructor
+@CrossOrigin
+public class ConversationController {
+    private final ConversationService conversationService;
+    @PostMapping(value = "/{user}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<ConversationDTO> createEmptyConversation(@PathVariable String user) {
+        try {
+            ConversationDTO conversation = conversationService.createEmptyConversation(user);
+            return new ResponseEntity<>(conversation, HttpStatus.CREATED);
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND); // user not found
+        } catch (ConversationAlreadyExistsException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST); //conversation already exists
+        }
+    }
+}
