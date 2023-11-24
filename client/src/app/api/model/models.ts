5e68ab78a756a965e9df31d93fe520743c0bf9e1
Ambre.Leveille <leveilleambre05@gmail.com>
2023-11-24 17:07:58 +0100

    avancement

---
+++ client/src/app/api/model/models.ts
@@ -0,0 +1,7 @@
+export * from './contactProfileDTO';
+export * from './conversationDTO';
+export * from './errorDTO';
+export * from './fullUserDTO';
+export * from './messageDTO';
+export * from './newMessageDTO';
+export * from './userDTO';
